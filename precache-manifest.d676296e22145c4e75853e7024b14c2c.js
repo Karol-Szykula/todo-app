ee0f0013c7a247b43faa69a7b4b40d673b548c78
Karol szykula <karol.szykula@gmail.com>
2018-11-27 14:53:54 +0100

    Updates

---
+++ precache-manifest.d676296e22145c4e75853e7024b14c2c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c92ec8fa081ed90a6e4",
+    "url": "/Karol-Szykula/todo-app/static/js/runtime~main.5c92ec8f.js"
+  },
+  {
+    "revision": "a827f218104a108931eb",
+    "url": "/Karol-Szykula/todo-app/static/js/main.a827f218.chunk.js"
+  },
+  {
+    "revision": "14d41437e1c26eb461ec",
+    "url": "/Karol-Szykula/todo-app/static/js/1.14d41437.chunk.js"
+  },
+  {
+    "revision": "8b95f8445002bd939fbbd6fde8048725",
+    "url": "/Karol-Szykula/todo-app/index.html"
+  }
+];
